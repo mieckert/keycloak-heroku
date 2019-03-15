a04caa29159b7310b52f67c97300afba8e5adc80
Michael Eckert <meckert@salesforce.com>
2019-03-15 12:42:57 +0100

    fixed regex

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Set database config from Heroku DATABASE_URL
-
 if [ "$DATABASE_URL" != "" ]; then
     echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
 
-    regex="^postgres://([a-zA-Z_-]):([a-zA-Z])@([a-z-]):([[:digit:]])/([a-zA-Z_-]*)$"
-    [[ $DATABASE_URL =~ $regex ]]
-    export DB_ADDR=${BASH_REMATCH[3]}
-    export DB_PORT=${BASH_REMATCH[4]}
-    export DB_DATABASE=${BASH_REMATCH[5]}
-    export DB_USER=${BASH_REMATCH[1]}
-    export DB_PASSWORD=${BASH_REMATCH[2]}
+    regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
+    if [[ $DATABASE_URL =~ $regex ]]; then
+        export DB_ADDR=${BASH_REMATCH[3]}
+        export DB_PORT=${BASH_REMATCH[4]}
+        export DB_DATABASE=${BASH_REMATCH[5]}
+        export DB_USER=${BASH_REMATCH[1]}
+        export DB_PASSWORD=${BASH_REMATCH[2]}
 
-    echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
+        echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
+    fi
+
 fi
-
 
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
