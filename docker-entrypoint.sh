ca94c78f9f1dd7594481f254d163b859a7a8cf9d
Michael Eckert <meckert@salesforce.com>
2019-03-15 11:53:10 +0100

    Logging for DATABASE_URL parsing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 # Set database config from Heroku DATABASE_URL
 
 if [ "$DATABASE_URL" != "" ]; then
+    echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
+
     regex="^postgres://([a-zA-Z_-]):([a-zA-Z])@([a-z-]):([[:digit:]])/([a-zA-Z_-]*)$"
     [[ $DATABASE_URL =~ $regex ]]
     export DB_ADDR=${BASH_REMATCH[3]}
@@ -10,6 +12,8 @@ if [ "$DATABASE_URL" != "" ]; then
     export DB_DATABASE=${BASH_REMATCH[5]}
     export DB_USER=${BASH_REMATCH[1]}
     export DB_PASSWORD=${BASH_REMATCH[2]}
+
+    echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
 fi
 
 
