2efb06f18a10c36971a3c222ecb4a35fb9142fc5
Jeff Tian <jeff.tian@outlook.com>
2020-06-11 12:57:21 +0800

    upgrade to 0.0.15 for wechat idp

---
+++ update-wechat-idp.sh
@@ -0,0 +1,2 @@
+VERSION=$1
+cp ~/jeff-tian/keycloak-services-social-weixin/target/keycloak-services-social-weixin-${VERSION}.jar ~/jeff-tian/keycloak-heroku/idps/wechat-mobile/keycloak-services-social-weixin.jar
