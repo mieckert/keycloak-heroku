2913abdea0857f6d4bb7862beb9f8232f0dbbecb
Jeff Tian <jeff.tian@outlook.com>
2022-03-07 10:13:54 +0800

    feat: add wecom idp

---
+++ update-wecom-idp.sh
@@ -0,0 +1,3 @@
+VERSION=$1
+echo "VERSION is ${VERSION}"
+cp "${HOME}/jeff-tian/keycloak-services-social-wechatwork/target/keycloak-services-social-wechat-work.jar" ~/jeff-tian/keycloak-heroku/idps/wecom/keycloak-services-social-wechat-work.jar
