912d2bebc9e039152438fbdba536347a7efc7744
cnjeftia <jeff.tian@outlook.com>
2023-03-09 10:49:39 +0800

    fix(UNI-133): add tag correctly

--- .github/dockerize.sh
+++ .github/dockerize.sh
@@ -1,6 +1,6 @@
 docker build -f Dockerfile.test -t jefftian/keycloak:"$1" .
 docker images
-docker run --network host -e CI=true -d -p 127.0.0.1:8080:8080 --name keycloak:"$1" jefftian/keycloak
+docker run --network host -e CI=true -d -p 127.0.0.1:8080:8080 --name keycloak jefftian/keycloak:"$1"
 docker ps | grep -q keycloak
 docker ps -aqf "name=keycloak$"
 docker push jefftian/keycloak:"$1"
