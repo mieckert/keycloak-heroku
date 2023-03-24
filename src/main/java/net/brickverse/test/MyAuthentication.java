ed8359fbba8245c029baf661379882dd30b928ca
Jeff Tian <jeff.tian@outlook.com>
2023-03-24 18:17:15 +0800

    feat: add my authenticator

---
+++ src/main/java/net/brickverse/test/MyAuthentication.java
@@ -0,0 +1,41 @@
+package net.brickverse.test;
+
+import org.jboss.logging.Logger;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+public class MyAuthentication implements org.keycloak.authentication.Authenticator {
+    private static final Logger logger = Logger.getLogger(MyAuthentication.class);
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+        logger.info("MyAuthentication.authenticate");
+        context.success();
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext context) {
+
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return false;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ src/main/java/net/brickverse/test/MyAuthenticationProviderFactory.java
@@ -0,0 +1,75 @@
+package net.brickverse.test;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+public class MyAuthenticationProviderFactory implements AuthenticatorFactory {
+    private static final MyAuthentication SINGLETON = new MyAuthentication();
+
+    @Override
+    public String getDisplayType() {
+        return "MyAuthentication";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return null;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return SINGLETON;
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "MyAuthentication";
+    }
+}
